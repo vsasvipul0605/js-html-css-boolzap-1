e3974dbc9743699bf93eaab5f8a04a9932332bbc
simone <sasso.96@libero.it>
2020-04-07 16:42:02 +0200

    milestone1

---
+++ main.js
@@ -0,0 +1,32 @@
+
+// aggiunta del messaggio digitato nel display quando clicco l'aereo
+$(".fa-paper-plane").click(
+  function () {
+    var msg = $(".input-msg-txt").val();
+    if (msg != "") {
+      $(".display-messaggi").append("<div class='messaggio-inviato'><p class='p-msg'>" + msg + "</p></div>");
+      $(".input-msg-txt").val("");
+    }
+
+  }
+)
+// evidenziare il contatto selezionato----------------
+$(".contatto").click(
+  function () {
+    $(this).addClass("contatto-attivo-bg");
+    $(this).siblings().removeClass("contatto-attivo-bg");
+  }
+)
+// cambio icona da microfono a aereo-----------------------
+$(".input-msg-txt").focus(function(){
+  $(".plane").css("display", "block");
+  $(".fa-microphone").css("display", "none");
+});
+$(".input-msg-txt").focusout(
+  send
+);
+function send() {
+  setTimeout(function(){
+    $(".plane").css("display", "none");
+    $(".fa-microphone").css("display", "block"); }, 200);
+}
